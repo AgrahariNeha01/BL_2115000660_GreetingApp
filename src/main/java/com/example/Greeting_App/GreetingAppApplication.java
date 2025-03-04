ad85462d858f2e2d360af25fe96f8d3c4ee41837
Neha Agrahari <neha16agrahari@gmail.com>
2025-03-04 11:37:57 +0530

    greeting

---
+++ src/main/java/com/example/Greeting_App/GreetingAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.Greeting_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Greetings!");
+		SpringApplication.run(GreetingAppApplication.class, args);
+	}
+
+}
